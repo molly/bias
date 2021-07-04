6064847353ddc3fa51385a1acfb11d5a7b34fc87
Molly White <molly.white5@gmail.com>
2021-07-04 18:40:34 -0400

    Parse references out of page HTML

---
+++ backend/constants/likely_not_primary_url.py
@@ -0,0 +1,8 @@
+LIKELY_NOT_PRIMARY_URL = {
+    "archive.today",
+    "doi.org",
+    "jstor.org",
+    "web.archive.org",
+    "webcitation.org",
+    "worldcat.org",
+}
--- backend/wikipedia/parse.py
+++ backend/wikipedia/parse.py
@@ -1,28 +1,62 @@
 from bs4 import BeautifulSoup
 from constants.misc import HEADERS
 from constants.urls import WIKIPEDIA_URL
+from constants.likely_not_primary_url import LIKELY_NOT_PRIMARY_URL
+from urllib.parse import urlparse
 import requests
 
 
-def get_references(soup):
+def get_references(soup, section_name):
+    """Find the references section."""
+    section_name = section_name.replace(" ", "_")
     references_header = soup.find(
-        lambda tag: tag.name == "h2" and tag.find("span", id="References")
+        lambda tag: tag.name == "h2" and tag.find("span", id=section_name)
     )
+    if not references_header:
+        return None
     references_list = references_header.find_next(["ol", "ul"])
-    return references_list.find_all("li")
+    if not references_list:
+        return None
+    return references_list.find_all("li", recursive=False)
 
 
-def evaluate(title):
-    # req = requests.get(WIKIPEDIA_URL + title, headers=HEADERS)
-    # html = req.text
-    with open("tmp.html", "r") as html_file:
-        html = html_file.read()
+def filter_domains(domains):
+    """If the possible domains list has more than one entry, filter out some domains
+    that are likely not the primary domain (for example, archived versions of the link,
+    DOI.org links, etc.)"""
+    if len(domains) == 1:
+        return domains
+    filtered = []
+    for anchor in domains:
+        if anchor["domain"] not in LIKELY_NOT_PRIMARY_URL:
+            filtered.append(anchor)
+    return filtered if len(filtered) > 0 else domains
+
+
+def parse_references(title, section_name="References"):
+    req = requests.get(WIKIPEDIA_URL + title, headers=HEADERS)
+    html = req.text
     soup = BeautifulSoup(html, "html.parser")
-    references = get_references(soup)
-    print(len(references))
+    html_references = get_references(soup, section_name)
+    if not html_references:
+        return None
+    references = {}
+    for ind, ref in enumerate(html_references):
+        anchors = ref.find_all("a", class_="external")
+        backlinks = ref.find("span", class_="mw-cite-backlink")
+        num_backlinks = len(backlinks.find_all("a")) if backlinks else 0
+        possible_domains = []
+        for anchor in anchors:
+            parsed = urlparse(anchor["href"])
+            netloc = (
+                parsed.netloc[4:] if parsed.netloc.startswith("www.") else parsed.netloc
+            )
+            possible_domains.append({"href": anchor["href"], "domain": netloc.lower()})
+        references[ind] = {
+            "id": ref.id,
+            "full": str(ref),
+            "possible_domains": filter_domains(possible_domains),
+            "usages": num_backlinks,
+        }
 
-
-if __name__ == "__main__":
-    # Temporary, just for testing
-    title = "Dinesh_D%27Souza"
-    evaluate(title)
+    return references
